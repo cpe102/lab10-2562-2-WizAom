c17f3bdaa83b0c861e6ee3bfdea5aaaa15402f12
WizAom <Aom_ll2555@hotmail.com>
2020-01-31 16:42:30 +0700

    1st

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+    srand(time(0));
+    string x[9]={"A","B+","B","C+","C","D+","D","F","W"};
+    int i,r;
+    cout << "Press Enter 3 times to reveal your future.\n";
+    for(i=1;i<=3;i++){
+        cin.get();
+    }
+    r = rand()%9;
+    cout << "You will get " << x[r] << " in this 261102.";
+    
+}
+
+
